e31239582dddded9e9089410629fab4d4234432c
Daniil Ermolaev <blcklptn@icloud.com>
2024-05-02 18:08:05 +0500

    new branch

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,23 @@ fn main() {
       }
     }
 
+    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (256, 256)).unwrap();
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..256 {
+            for x in 0..256 {
+                let offset = y*pitch + x*3;
+                buffer[offset + 0] = x as u8;
+                buffer[offset + 1] = y as u8;
+                buffer[offset + 2] = 0;
+            }
+        }
+    }).unwrap();
+
+    renderer.clear();
+    renderer.copy(&texture, None, Some(Rect::new(100, 100, 256, 256)));
+    renderer.copy_ex(&texture, None, Some(Rect::new(450, 100, 256, 256)), 30.0, None, (false, false));
+    renderer.present();
 
     imgui_sdl2.prepare_frame(imgui.io_mut(), &window, &event_pump.mouse_state());
 
---
+++ src/new_main.rs
@@ -0,0 +1,23 @@
+extern crate imgui_sdl2;
+extern crate gl;
+extern crate imgui_opengl_renderer;
+
+mod geita_ui;
+
+use std::time::Instant;
+
+use imgui::{ Condition, FontSource, FontGlyphRanges,
+            Style, StyleVar, Context };
+
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+pub struct CoreWindow {
+    sdl_context: Context,
+    video: 
+}
+
+fn main() {
+    
+}
