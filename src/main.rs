347ee80b7ae542179d23fdc2183e73f30ff9a09c
Daniil Ermolaev <blcklptn@icloud.com>
2024-05-12 11:13:18 +0500

    Now we can draw images.

--- src/main.rs
+++ src/main.rs
@@ -33,11 +33,19 @@ pub struct Entity2D<'a> {
     pub position: ImVec2,
 }
 
-fn draw_2d_cube(canvas: &mut Canvas<sdl2::video::Window>, position: &mut [i32; 2]) {
-    let texture_creator = canvas.texture_creator();
+fn draw_image(canvas: &mut Canvas<sdl2::video::Window>, position: &mut [i32; 2], texture_creator: &TextureCreator<WindowContext>) {
+    let surface = Surface::new(512, 512, PixelFormatEnum::RGB24).unwrap();
+    let png = Path::new("/mnt/c/Users/oksan/OneDrive/Documents/GitHub/geita_project/Fortnight-resources/Assets/hollow_knight_LARGE_ICO.png");
+    let texture = texture_creator.load_texture(png).unwrap();
+    canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
+    let rect1 = Rect::new(position[0], position[1], 250, 250);
+    canvas.copy(&texture, None, rect1).unwrap();
+}
+
+fn draw_2d_cube(canvas: &mut Canvas<sdl2::video::Window>, position: &mut [i32; 2], texture_creator: &TextureCreator<WindowContext>) {
     let surface = Surface::new(512, 512, PixelFormatEnum::RGB24).unwrap();
     let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::RGB24, 512, 512 ).unwrap();
-    
+
     texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
         for y in (0..256) {
             for x in (0..256) {
@@ -48,6 +56,7 @@ fn draw_2d_cube(canvas: &mut Canvas<sdl2::video::Window>, position: &mut [i32; 2
             }
         }
     }).unwrap();
+    
 
     canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
     let rect1 = Rect::new(position[0], position[1], 250, 250);
@@ -59,16 +68,9 @@ fn main() {
     let mut window = CoreWindow::new("Project Manager".to_string());
     let mut canvas = window.window.into_canvas().build().unwrap();
     let mut event_pump = window.sdl_context.event_pump().unwrap();
-    //let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (256, 256)).unwrap();
     let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
     let texture_creator = canvas.texture_creator();
-    let png = Path::new("/Users/twofacedjanus/Documents/geita_project/Fortnight-resources/Assets/hollow_knight_LARGE_ICO.png");
-    let texture = texture_creator.load_texture(png).unwrap();
-
-    canvas.copy(&texture, None, None);
-    canvas.present();
-    
-    
+   
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -85,8 +87,8 @@ fn main() {
         canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         canvas.clear();
 
-        draw_2d_cube(&mut canvas, &mut [300i32,300i32]);
-        draw_2d_cube(&mut canvas, &mut [700i32,700i32]);
+        draw_2d_cube(&mut canvas, &mut [300i32,300i32], &texture_creator);
+        draw_image(&mut canvas, &mut [700i32,700i32], &texture_creator);
 
         canvas.present();
     }
